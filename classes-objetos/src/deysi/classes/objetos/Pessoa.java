9c67b439725515cc119c4df7734cf33c61bb9f44
DeysiLopes <deysilinconl@gmail.com>
2022-04-19 20:37:52 -0300

    Add some exercises made with teacher

---
+++ classes-objetos/src/deysi/classes/objetos/Pessoa.java
@@ -0,0 +1,12 @@
+package deysi.classes.objetos;
+
+public class Pessoa {
+	String nome;
+	String cpf;
+	String endereco;
+	
+	Pessoa(String cpf, String nome){
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+}
---
+++ classes-objetos/src/deysi/classes/objetos/SistemaCadastro.java
@@ -0,0 +1,10 @@
+package deysi.classes.objetos;
+
+public class SistemaCadastro {
+	public static void main(String[] args) {
+		Pessoa deysi = new Pessoa("8989898", "deysi");
+		
+		System.out.println(deysi.nome);
+		System.out.println(deysi.cpf);
+	}
+}
